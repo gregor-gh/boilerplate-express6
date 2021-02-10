4cbc66f38dff4965fab641b76483792c3e184922
gregor-gh <gregor@live.co.uk>
2021-02-10 20:31:25 +0000

    Modified myApp.js
    Added express.static middle ware to serve public assets dir

--- myApp.js
+++ myApp.js
@@ -5,9 +5,13 @@ var app = express();
 
 //app.get("/",(req,res)=> res.send("Hello Express"));
 
+// Normal usage
+app.use("/public",express.static(__dirname + "/public"));
+
 app.get("/", (req,res) => {
     res.sendFile(__dirname+"/views/index.html");
 });
+
 
 
 
