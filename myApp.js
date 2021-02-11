49b0323ae7c41be5aa4e4d375e6bf6ce685a5500
gregor-gh <gregor@live.co.uk>
2021-02-11 20:07:35 +0000

    Added /now route returning current datetime

--- myApp.js
+++ myApp.js
@@ -16,6 +16,13 @@ app.use("/", (req,res,next) => {
     next();
 })
 
+app.get("/now",(req,res,next) => {
+    req.time = new Date().toString();
+    next();
+}, (req,res) => {
+    res.send({"time": req.time})
+})
+
 app.get("/", (req,res) => {
     res.sendFile(__dirname+"/views/index.html");
 });
